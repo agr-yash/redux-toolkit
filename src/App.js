71e8f239b1a7a9e767bccb9c32c2fe44540c278d
Yash Agarwal <yashagr@Yashs-MacBook-Air.local>
2023-03-02 14:26:09 +0530

    change increment val to 3

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
 
   return (
     <div className="App">
-      <div onClick={() => handleChangeCounter(2)}>increment</div>
+      <div onClick={() => handleChangeCounter(3)}>increment</div>
       <div>{count}</div>
-      <div onClick={() => handleChangeCounter(-2)}>decrement</div>
+      <div onClick={() => handleChangeCounter(-3)}>decrement</div>
     </div>
   );
 }
